a698eff413cb7e4b85d4377e5b931211c23db27c
vikash kumar <vikash20032103@gmail.com>
2025-02-25 16:10:32 +1300

    Comments added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 
 function App() {
   return (
+    // This is a comment
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
